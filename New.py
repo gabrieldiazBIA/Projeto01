0c9fe1d552b194c75ace31c058921474c85008e7
Gabriel Dias <gabrieldiazcvc@gmail.com>
2025-07-03 21:40:06 -0300

    Primeiro commit após github conectado

--- New.py
+++ New.py
@@ -2,3 +2,4 @@ print("Mensagem aleatoria")
 print("Nova funcionalidade adicionada")
 print("Primeiro commit dentro da branch de nova funcionalidade.")
 print("Alterações dentro da branch nova funcionlidade")
+#Primeira linha após conectar ao github. 
